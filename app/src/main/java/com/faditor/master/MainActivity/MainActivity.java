5e499f5c16f459b6700f8acf646f56fbe5446476
ccomangi2 <ccomangi2@gmail.com>
2020-08-24 19:14:07 +0900

    MainActivity 오늘의 베스트룩, 인기 페디터 사진 구현

--- app/src/main/java/com/faditor/master/MainActivity/MainActivity.java
+++ app/src/main/java/com/faditor/master/MainActivity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.faditor.master.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // 타이틀 보여줄지 말지 결정
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
     }
 }
--- app/src/main/java/com/faditor/master/SettingActivity/SettingActivity.java
+++ app/src/main/java/com/faditor/master/SettingActivity/SettingActivity.java
@@ -1,8 +1,11 @@
 package com.faditor.master.SettingActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 import com.faditor.master.R;
 
@@ -12,5 +15,21 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.setting_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // 타이틀 보여줄지 말지 결정
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
