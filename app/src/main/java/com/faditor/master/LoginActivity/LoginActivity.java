21521971084bf2aa0a350a368d61526acba57b83
ccomangi2 <ccomangi2@gmail.com>
2020-08-27 23:49:42 +0900

    페이스북 소셜 로그인

--- app/src/main/java/com/faditor/master/LoginActivity/LoginActivity.java
+++ app/src/main/java/com/faditor/master/LoginActivity/LoginActivity.java
@@ -2,19 +2,40 @@ package com.faditor.master.LoginActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
-import com.faditor.master.MainActivity.MainActivity;
+import com.facebook.login.widget.LoginButton;
 import com.faditor.master.R;
 
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity {
+    private LoginButton btn_facebook_login;
+
+    private LoginCallback mLoginCallback;
+    private CallbackManager mCallbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mCallbackManager = CallbackManager.Factory.create();
+        mLoginCallback = new LoginCallback();
+
+        btn_facebook_login = (LoginButton) findViewById(R.id.btn_facebook_login);
+        btn_facebook_login.setReadPermissions(Arrays.asList("public_profile", "email"));
+        btn_facebook_login.registerCallback(mCallbackManager, mLoginCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/faditor/master/MainActivity/MainActivity.java
+++ app/src/main/java/com/faditor/master/MainActivity/MainActivity.java
@@ -7,7 +7,12 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ActionMenuView;
@@ -21,6 +26,9 @@ import com.faditor.master.R;
 import com.faditor.master.SearchActivity.SearchActivity;
 import com.faditor.master.SettingActivity.SettingActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
 
+        getHashkey();
     }
         class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
             @Override
@@ -64,5 +73,25 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return true;
             }
+    }
+    private void getHashkey() {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
     }
 }
